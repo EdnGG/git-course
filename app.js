10d08b08e0cbc9b71935c9d5eae55f13bbf8b3db
Eden <gresseden@gmail.com>
2020-10-07 23:07:39 -0400

    learning how to iterating an array of objects

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-console.log('Adding some JS')
-const props = [
+console.log('linking app.js to index.html')
+let props = [
   {
     reservation: '001',
-    name: 'John',
+    name: 'Steven',
     lastName: 'Doe',
     eta: '15:00 hrs',
     status: 'Confirmado', //map
   },
   {
     reservation: '002',
-    name: 'John',
+    name: 'Victor',
     lastName: 'Doe',
     eta: '15:00 hrs',
     status: 'No Confirmado', //map
   },
   {
     reservation: '003',
-    name: 'John',
+    name: 'Brandon',
     lastName: 'Doe',
     eta: '15:00 hrs',
     status: 'No Confirmado', //map
   },
   {
     reservation: '004',
-    name: 'John',
+    name: 'Carlos',
     lastName: 'Doe',
     ETA: '15:00 hrs',
     status: 'Cancelado', //map
   },
   {
     reservation: '005',
-    name: 'John',
+    name: 'David',
     lastName: 'Doe',
     eta: '15:00 hrs',
     status: 'Confirmado', //map
@@ -43,6 +43,11 @@ const props = [
     status: 'Cancelado', //map
   },
 ]
-
-props.map((ele) => return ele.name)
-  
+const filtering = () => {
+  let paragraph = document.querySelector("#paragraph")
+  const newArray = props.filter((el) => {
+    if (el.name === 'John') {
+      return paragraph.innerHTML = el.name
+    }
+  })
+}
